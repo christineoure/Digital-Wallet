1e5f8c8434a9c78c2f70d622252673d09c8f906e
christineoure <ourechristine@gmail.com>
2022-08-25 21:58:09 +0300

    add table changes

--- wallet/admin.py
+++ wallet/admin.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from.models import Account, Card, Currency, Customer, Loan, Notification, Reward, Third_party, Transaction, Wallet
+from.models import Account, Card, Currency, Customer, Loan, Notification, Receipt, Reward, Third_party, Transaction, Wallet
 
 # Register your models here.
 
@@ -12,12 +12,74 @@ class CustomerAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Customer,CustomerAdmin)
-admin.site.register(Wallet,)
-admin.site.register(Account,)
-admin.site.register(Transaction, )
-admin.site.register(Card, )
-admin.site.register(Third_party, )
-admin.site.register(Notification,)
-admin.site.register(Loan,)
-admin.site.register(Reward,)
-admin.site.register(Currency,)
+
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "customer", "balance",)
+    search_fields = ("user_name", "customer",)
+
+
+admin.site.register(Wallet, WalletAdmin)
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ("account_name", "account_type", "account_number",)
+    search_fields = ("account_name", "account_type",)
+
+
+admin.site.register(Account, AccountAdmin)
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ("amount", "transaction_type", "transaction_code", )
+    search_fields = ("amount", "account_type",)
+
+
+admin.site.register(Transaction, TransactionAdmin)
+
+class CardAdmin(admin.ModelAdmin):
+    list_display = ("card_number", "card_status", "card_name",)
+    search_fields = ("card_number", "card_status",)
+
+
+admin.site.register(Card, CardAdmin)
+
+
+class Third_partyAdmin(admin.ModelAdmin):
+    list_display = ("full_name", "email", "phone_number")
+    search_fields = ("full_name", "email",)
+
+    
+admin.site.register(Third_party, Third_partyAdmin)
+
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ("datetime", "statement", "receipt")
+    search_fields = ("datetime", "statement",)
+
+
+admin.site.register(Notification, NotificationAdmin)
+
+
+class LoanAdmin(admin.ModelAdmin):
+    list_display = ("account", "loan_type", "loan_term", "guarantor")
+    search_fields = ("account", "loan_type",)
+
+
+admin.site.register(Loan, LoanAdmin)
+
+
+class ReceiptAdmin(admin.ModelAdmin):
+    list_display = ("datetime", "file", "amount")
+    search_fields = ("datetime", "file",)
+
+
+admin.site.register(Receipt, ReceiptAdmin)
+
+class RewardAdmin(admin.ModelAdmin):
+    list_display = ("wallet", "points", "transaction")
+    search_fields = ("wallet", "transaction",)
+
+
+admin.site.register(Reward, RewardAdmin)
+
+class CurrencyAdmin(admin.ModelAdmin):
+    list_display = ("country", "name", "symbol")
+    search_fields = ("country", "name",)
+admin.site.register(Currency, CurrencyAdmin)
